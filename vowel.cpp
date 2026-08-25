f713fe2cd96c0fe30829b4164c6b1bbf3ed9aa48
Ananya Rajput <ananya13072007@gmail.com>
2026-08-25 15:46:47 +0530

    Fix formatting and add vowel detection in C++

--- vowel.cpp
+++ vowel.cpp
@@ -1,1 +1,23 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    cout << "Vowels are: ";
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U') {
+
+            cout << str[i] << " ";
+        }
+    }
+
+    return 0;
+}

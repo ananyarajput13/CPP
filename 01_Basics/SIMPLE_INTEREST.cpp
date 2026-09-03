383c0d17c9529bc2b52df0b17d2e469e6e50ea45
ananyarajput13 <ananya13072007@gmail.com>
2026-09-03 22:26:22 +0530

    add SIMPLE_INTEREST.cpp

---
+++ 01_Basics/SIMPLE_INTEREST.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float P, R, T, SI;
+
+    cout << "Enter Principal amount: ";
+    cin >> P;
+
+    cout << "Enter Rate of interest: ";
+    cin >> R;
+
+    cout << "Enter Time: ";
+    cin >> T;
+
+    SI = (P * R * T) / 100;
+
+    cout << "Simple Interest = " << SI;
+
+    return 0;
+}

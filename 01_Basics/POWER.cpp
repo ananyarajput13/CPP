425ee221e3c074e7d9f20775380f4308e4b882d7
ananyarajput13 <ananya13072007@gmail.com>
2026-09-07 22:59:10 +0530

    add POWER.cpp

---
+++ 01_Basics/POWER.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int base, exponent, result = 1;
+
+    cout << "Enter base: ";
+    cin >> base;
+
+    cout << "Enter exponent: ";
+    cin >> exponent;
+
+    for(int i = 1; i <= exponent; i++)
+    {
+        result = result * base;
+    }
+
+    cout << "Power = " << result;
+
+    return 0;
+}

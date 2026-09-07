4065a58bc02c2dae658e73a0ec9fd17e75973b90
ananyarajput13 <ananya13072007@gmail.com>
2026-09-07 23:07:57 +0530

    add GCD_OF_TWO_NO.cpp

---
+++ 01_Basics/GCD_OF_TWO_NO.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, gcd;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    while(b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    gcd = a;
+
+    cout << "GCD = " << gcd;
+
+    return 0;
+}

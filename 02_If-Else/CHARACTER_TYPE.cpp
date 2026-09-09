0ca14d914deea8be75a1faf55889f5c68dd21858
ananyarajput13 <ananya13072007@gmail.com>
2026-09-09 22:18:21 +0530

    add CHARACTER_TYPE.cpp

---
+++ 02_If-Else/CHARACTER_TYPE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch >= 'A' && ch <= 'Z')
+        cout << "Uppercase Character";
+    else if (ch >= 'a' && ch <= 'z')
+        cout << "Lowercase Character";
+    else if (ch >= '0' && ch <= '9')
+        cout << "Digit";
+    else
+        cout << "Special Character";
+
+    return 0;
+}

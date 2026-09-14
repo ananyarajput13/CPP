dd2d3f01b62e7a5fa24b522e23ff4d3eb9c51b67
ananyarajput13 <ananya13072007@gmail.com>
2026-09-14 23:30:28 +0530

    add UPPERCASE_LOWERCASE.cpp

---
+++ 02_If-Else/UPPERCASE_LOWERCASE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch >= 'A' && ch <= 'Z')
+        cout << "Uppercase";
+    else if (ch >= 'a' && ch <= 'z')
+        cout << "Lowercase";
+    else
+        cout << "Not an Alphabet";
+
+    return 0;
+}

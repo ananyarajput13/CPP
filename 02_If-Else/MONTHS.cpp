a6b07cc269d56d688d5758bba3f74790b570a3aa
ananyarajput13 <ananya13072007@gmail.com>
2026-09-15 23:12:57 +0530

    add MONTHS.cpp

---
+++ 02_If-Else/MONTHS.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int month;
+
+    cout << "Enter month number (1-12): ";
+    cin >> month;
+
+    if (month == 1)
+        cout << "31 Days";
+    else if (month == 2)
+        cout << "28 Days";
+    else if (month == 3)
+        cout << "31 Days";
+    else if (month == 4)
+        cout << "30 Days";
+    else if (month == 5)
+        cout << "31 Days";
+    else if (month == 6)
+        cout << "30 Days";
+    else if (month == 7)
+        cout << "31 Days";
+    else if (month == 8)
+        cout << "31 Days";
+    else if (month == 9)
+        cout << "30 Days";
+    else if (month == 10)
+        cout << "31 Days";
+    else if (month == 11)
+        cout << "30 Days";
+    else if (month == 12)
+        cout << "31 Days";
+    else
+        cout << "Invalid Month Number";
+
+    return 0;
+}

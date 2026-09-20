58b740bf531cd57b8f6a4bdc612ef79279db681b
ananyarajput13 <ananya13072007@gmail.com>
2026-09-20 23:34:29 +0530

    add COUNT_DIGITS_OF_AN_INTEGER.cpp

---
+++ 03_Loops/WHILE_LOOP/COUNT_DIGITS_OF_AN_INTEGER.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    if (n == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            n = n / 10;
+            count++;
+        }
+    }
+
+    cout << "Number of digits = " << count;
+
+    return 0;
+}
